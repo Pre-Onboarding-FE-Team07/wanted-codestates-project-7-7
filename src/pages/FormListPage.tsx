8a2301e3b75c997bdb1ca582c966e8d5c5cbc564
Lela12 <dbcocotn4@hanmail.net>
2022-03-18 19:52:05 +0900

    Feat: FormListPage UI구현

--- src/pages/FormListPage.tsx
+++ src/pages/FormListPage.tsx
@@ -1,24 +1,32 @@
 import styled from 'styled-components';
 import { Pagination } from 'antd';
 import AddButton from 'components/Main/AddButton';
-import FormList from 'components/Main/FormList';
+import DropdownList from 'components/Main/DropdownList';
+import { FormListContext } from 'context/FormListContext';
+import { FormDataStateType } from 'interfaces/createForm.d';
+import { useContext, useMemo } from 'react';
 
 function FormListPage() {
+  const { formListState } = useContext(FormListContext);
+  const formList = useMemo(() => formListState.formList, [formListState]);
   return (
-    // <FormListProvider>
     <Container>
       <Wrapper>
         <Label>최신목록</Label>
         <AddButton />
       </Wrapper>
       <FormCard>
-        <FormList />
+        {formList?.map((form: FormDataStateType) => (
+          <li key={form.id}>{form.title}</li>
+        ))}
+        <DropDownWrap>
+          <DropdownList />
+        </DropDownWrap>
       </FormCard>
       <PageContainer>
         <Pagination defaultCurrent={1} total={50} />
       </PageContainer>
     </Container>
-    // </FormListProvider>
   );
 }
 
@@ -49,11 +57,18 @@ const Label = styled.label`
 const FormCard = styled.ul`
   width: 100%;
   height: 100%;
-  /* height: 5rem;
+  height: 5rem;
   border: 1px solid black;
   list-style: none;
   margin: 1rem 0;
-  position: relative; */
+  position: relative;
+`;
+
+const DropDownWrap = styled.div`
+  display: flex;
+  position: absolute;
+  top: 16px;
+  right: 10px;
 `;
 
 const PageContainer = styled.footer`
