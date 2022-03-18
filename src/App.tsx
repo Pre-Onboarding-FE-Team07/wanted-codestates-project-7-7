0420df0f280caed87bb67d596affcd87d51e4e8f
ye-yo <yeyo0x0@gmail.com>
2022-03-18 17:13:03 +0900

    Refactor: pages 모듈화 및 미제거된 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
-import FormListPage from './pages/FormListPage';
-import CreateFormPage from './pages/CreateFormPage';
-import WriteFormPage from './pages/WriteFormPage';
-import UserDataPage from './pages/UserDataPage';
+import { FormListPage, CreateFormPage, WriteFormPage, UserDataPage } from './pages/index';
 import FormListProvider from 'context/FormListContext';
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
--- src/components/Field/FieldTools/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CheckBox } from './CheckBox';
-export { default as Input } from './Input';
-export { default as OptionList } from './OptionList';
-export { default as SelectType } from './SelectType';
