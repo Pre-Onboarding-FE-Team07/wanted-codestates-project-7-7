bc6af680865b14330cdb95688f11bd7953ebc54f
HyoEun Ye <50618754+ye-yo@users.noreply.github.com>
2022-03-20 17:00:49 +0900

    Feat: 폼 미리보기 화면 완성 (#37)
    * Feat: writeForm 분리
    
    Related to #28
    
    * Fix: SelectBox 컴포넌트 key 추가

---
+++ src/components/WrtieForm/index.ts
@@ -0,0 +1,6 @@
+export { default as Agreement } from './Agreement';
+export { default as File } from './File';
+export { default as Name } from './Name';
+export { default as Phone } from './Phone';
+export { default as PostCode } from './PostCode';
+export { default as SelectBox } from './SelectBox';
